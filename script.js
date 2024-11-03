261413c6bbc546b93194181592d11dde3ecab8c8
RemakesZ <remakes.contact@gmail.com>
2024-11-03 14:29:27 +0200

    fix

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function togglesidebar() {
     const toggleSidebar =document.getElementById("sidebar");
     const sidebarButtons = document.querySelectorAll (".sidebar ul li a");
-    const sidebarButtonDesc = document.querySelectorAll (".sidebar ul li span");
+    const sidebarButtonDesc = document.querySelectorAll (".sidebar ul li a span");
     if (sidebar.style.width === '0px') {
         sidebar.style.width = '230px';
         sidebarButtonDesc.style.display = "inline-block";
