ffdb69c9daf7c927b02d53094b8e029f5b3cf221
RemakesZ <remakes.contact@gmail.com>
2024-11-03 03:03:59 +0200

    make it invis???

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 function togglesidebar() {
-    const togglesidebar =document.getElementById("sidebar");
+    const toggleSidebar =document.getElementById("sidebar");
+    const sidebarButtons = document.querySelectorAll (".sidebar ul li a")
+    const sidebarButtonDesc = document.querySelectorAll (".sidebar ul li span")
     if (sidebar.style.width === '0px') {
         sidebar.style.width = '230px'
+        span.style.opacity = '1';
+
+        
     }
     else{
         sidebar.style.width = 0
-    }
+        span.style.opacity = '0';
+        }
 }
