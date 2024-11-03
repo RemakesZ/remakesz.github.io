ad3dc100386bc917e950771959e324bfe8146e78
RemakesZ <remakes.contact@gmail.com>
2024-11-03 13:22:18 +0200

    please work js

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 function togglesidebar() {
     const toggleSidebar =document.getElementById("sidebar");
-    const sidebarButtons = document.querySelectorAll (".sidebar ul li a")
-    const sidebarButtonDesc = document.querySelectorAll (".sidebar ul li span")
+    const sidebarButtons = document.querySelectorAll (".sidebar ul li a");
+    const sidebarButtonDesc = document.querySelectorAll (".sidebar ul li span");
     if (sidebar.style.width === '0px') {
-        sidebar.style.width = '230px'
-        sidebarButtonDesc.style.display = "none";
-
-        
+        sidebar.style.width = '230px';
+        sidebarButtonDesc.style.display = "inline-block";
+        sidebarButtons.style.borderRadius = "50%";
     }
     else{
         sidebar.style.width = 0
-        sidebarButtonDesc.style.display = "all";
+        sidebarButtonDesc.style.display = "none";
+        sidebarButtons.style.borderRadius = "0.5rem";
+
         }
 }
