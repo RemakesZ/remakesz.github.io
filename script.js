9cfa3f35344fd7138dbaad1025e7916d66d1e4b5
RemakesZ <remakes.contact@gmail.com>
2025-01-05 00:12:35 +0200

    sidebar toggle exp1

--- script.js
+++ script.js
@@ -4,13 +4,16 @@ function togglesidebar() {
     const sidebarButtonDesc = document.querySelectorAll (".sidebar ul li a span");
     if (sidebar.style.width === '0px') {
         sidebar.style.width = '230px';
-        sidebarButtonDesc.style.display = "inline-block";
         sidebarButtons.style.borderRadius = "50%";
+        sidebarButtonDesc.forEach((span) => {
+            span.style.display = span.style.display === "inline-block" ? "block" : "inline-block";
+              });
     }
     else{
         sidebar.style.width = 0
-        sidebarButtonDesc.style.display = "none";
         sidebarButtons.style.borderRadius = "0.5rem";
-
+        sidebarButtonDesc.forEach((span) => {
+        span.style.display = span.style.display === "none" ? "block" : "none";
+          });
         }
-}
+    }
