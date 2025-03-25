0dee6b4551f462e3e4c32ed9e388d2bf72b4185b
RemakesZ <remakes.contact@gmail.com>
2025-03-25 05:29:54 +0200

    who knows

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
 function togglesidebar() {
     const sidebarWhole =document.getElementById("sidebar");
-    const sidebarButtons = document.querySelectorAll (".sidebar ul li a");
-    const sidebarButtonsAll = document.querySelectorAll (".sidebar ul");
+    const sidebarButtonsAll = document.getElementById ("SidebarUl");
     if (sidebar.style.width === '0px') {
         sidebar.style.width = '230px';
         sidebar.style.padding = ".4rem .8rem";
-        sidebarButtons.style.borderRadius = "50%";
         sidebarButtonsAll.style.display = "flex";
     }
     else{
         sidebar.style.width = 0
         sidebar.style.padding = 0 
-        sidebarButtons.style.borderRadius = "0.5rem";
         sidebarButtonsAll.style.display = "none";
-
         }
     }
